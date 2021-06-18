db45b85387286d1d153cd5c36f8c5302ee07f6c8
FernandoGutierrez23 <vi11089@innovaccion.mx>
2021-06-17 23:37:51 -0500

    SegundoCommit: Captcha terminado

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -61,6 +61,18 @@
                             </div>
                         </div>
 
+                        
+                        <!-- CODIGO PARA EL CAPTCHA -->
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                {!! NoCaptcha::renderJs() !!}
+                                {!! NoCaptcha::display() !!}
+                                @error('g-recaptcha-response')
+                                   <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                                @enderror
+                            </div>
+                        </div>
+
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
